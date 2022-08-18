22f8c5538ba0505e5031414fc87b9409e0bb37e0
Victor Azevedo <josevcazevedo@gmail.com>
2022-08-18 16:18:11 -0300

    implementado todos requisitos

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,149 @@
-"use strict";
-
 // *****************************************
-// Variaveis e Funcoes Auxiliares
+// *****************************************
+// Funcoes Auxiliares
 // *****************************************
 //
-let delayInMilliseconds = 500;
-
+// Funcao para verificar se eh Par (Even)
 const isEven = (number) => (number % 2 == 0 ? true : false);
 
-// Funcao para deixar Array aleatoria
+// Funcao para alterar ordem da Array
 function comparador() {
   return Math.random() - 0.5;
 }
 
-// Retorna o nome da carta (gif)
+// Funcao que retorna o nome da carta (gif)
 const parrotName = (card) =>
   card.querySelector(".parrot-back").getAttribute("src");
 
 // *****************************************
-// Selecao e distribuicao das cartas
+// *****************************************
+// Funcoes para Inicializar Jogo e Distribuir as cartas
 // *****************************************
 //
+// Funcao para iniciar o jogo
+function startParrotGame() {
+  numberOfCards = loadNumberCards();
+  dealCards(numberOfCards);
+
+  // Variaveis de Controle
+  roundsCounter = 0;
+  isFirstCard = true;
+  firstCardName = 0;
+  secondCardName = 0;
+  flagWait = false;
+}
+
+// Ler quantidade de cartas
+function loadNumberCards() {
+  let nCards = 0;
+  while (!(isEven(nCards) && nCards >= nCardsMin && nCards <= nCardMax)) {
+    nCards = Number(
+      prompt(
+        `Informe a quantidade de cartas que deseja jogar (numeros pares de ${nCardsMin} a ${nCardMax})`
+      )
+    );
+  }
+  return nCards;
+}
+
+// Criar, Embaralhar e Distribuir  o total de cartas
+function dealCards(numberOfCards) {
+  const nPair = numberOfCards / 2;
+  const parrotsGifGame = parrotsGif.sort(comparador).slice(0, nPair);
+  const cardsShuffled = parrotsGifGame.concat(parrotsGifGame).sort(comparador);
+  //Distribuir cartas no tabuleiro
+  const tabuleiro = document.querySelector(".container");
+  for (let i = 0; i < numberOfCards; i++) {
+    tabuleiro.innerHTML += `
+    <div class="card" onclick="checkCard(this)">
+      <div class="front-face face">
+        <img
+          class="parrot-front"
+          src="./images/parrot.png"
+          alt="a parrot"
+        />
+      </div>
+      <div class="back-face face">
+        <img
+          class="parrot-back"
+          src="./images/${cardsShuffled[i]}"
+          alt="a parrot's gif"
+        />
+      </div>
+    </div>`;
+  }
+}
+
+// *****************************************
+// *****************************************
+// Funcoes para as egras do jogo
+// *****************************************
+
+// Virar a carta
+function turnCard(card) {
+  card.classList.add("turn");
+}
+
+// Desvirar as catas que nao deram Match
+function unturnCard(card) {
+  card.classList.remove("turn"); //desvira carta atual
+  document.querySelector(".turn").classList.remove("turn"); //desvira a primeira carta da rodada
+  flagWait = false; // libera virar apos desvirar as 2 cartas
+}
+
+// Marcar as cartas que deram Match
+function matchCard(card) {
+  const cardsMatched = document.querySelectorAll(".turn");
+  for (let i = 0; i < cardsMatched.length; i++) {
+    cardsMatched[i].classList.remove("turn");
+    cardsMatched[i].classList.add("match");
+  }
+  const totalMatched = document.querySelectorAll(".match").length;
+  if (totalMatched === numberOfCards) {
+    setTimeout(endOfGame, oneSecond);
+  }
+}
+
+// Verifica carta clicada
+function checkCard(card) {
+  //Evita virar a 3 carta
+  if (!flagWait) {
+    // Considera apenas os clicks de cartas que nao formaram par nem estao viradas
+    if (!card.classList.contains("match") && !card.classList.contains("turn")) {
+      turnCard(card);
+      if (isFirstCard) {
+        firstCardName = parrotName(card);
+        console.log(`1 - ${firstCardName}`);
+        isFirstCard = false;
+      } else {
+        secondCardName = parrotName(card);
+        console.log(`2 - ${secondCardName}`);
+        if (firstCardName === secondCardName) {
+          matchCard(card);
+        } else {
+          flagWait = true;
+          console.log(flagWait);
+          setTimeout(unturnCard, oneSecond, card);
+        }
+        roundsCounter++; //incremeta round
+        isFirstCard = true; //condicao para proxima rodada
+      }
+    }
+  }
+}
+
+function endOfGame() {
+  alert(`Você ganhou em ${roundsCounter} jogadas!`);
+}
+
+// *****************************************
+// Variaveis Auxiliares
+// *****************************************
+const oneSecond = 1000;
+
+// *****************************************
+// Variaveis Controle do Jogo
+// *****************************************
 // Cartas do jogo:
 const parrotsGif = [
   "60fps_parrot.gif",
@@ -31,104 +154,27 @@ const parrotsGif = [
   "everythings_fine_parrot.gif",
   "redhatparrot.gif",
 ];
+
+// Numero de cartas permitido
 const nCardsMin = 4;
 const nCardMax = parrotsGif.length * 2;
-let nCards = 0;
 
-// Leitura da qnte de cartas
-while (!(isEven(nCards) && nCards >= nCardsMin && nCards <= nCardMax)) {
-  nCards = Number(
-    prompt(
-      `Informe a quantidade de cartas que deseja jogar (numeros pares de ${nCardsMin} a ${nCardMax})`
-    )
-  );
-}
+// Numero de cartas da rodada
+let numberOfCards = 0;
 
-// Criar e Embaralhar o total de cartas
-const nPair = nCards / 2;
-const parrotsGifGame = parrotsGif.sort(comparador).slice(0, nPair);
-const cardsShuffled = parrotsGifGame.concat(parrotsGifGame).sort(comparador);
-
-//Distribuir cartas no tabuleiro
-const tabuleiro = document.querySelector(".container");
-for (let i = 0; i < nCards; i++) {
-  tabuleiro.innerHTML += `
-  <div class="card" onclick="checkCard(this)">
-    <div class="front-face face">
-      <img
-        class="parrot-front"
-        src="./images/parrot.png"
-        alt="a parrot"
-      />
-    </div>
-    <div class="back-face face">
-      <img
-        class="parrot-back"
-        src="./images/${cardsShuffled[i]}"
-        alt="a parrot's gif"
-      />
-    </div>
-  </div>`;
-}
-
-// *****************************************
-// Funcoes e variaveis para regras do jogo
-// *****************************************
-//
 // Variaveis de Controle
 let roundsCounter = 0;
 let isFirstCard = true;
 
-// Vira a carta
-function turnCard(card) {
-  card.classList.add("turn");
-}
+// Variaveis para  identificar as cartas de cada viradas a cada rodada
+let firstCardName = 0;
+let secondCardName = 0;
 
-// Desvira as catas que nao deram Match
-function unturnCard(card) {
-  card.classList.remove("turn"); //desvirar carta atual
-  document.querySelector(".turn").classList.remove("turn"); //desvira a primeira carta da rodada
-}
+let flagWait = false; // Evita virar 3 cartas
 
-// Marca as cartas que deram Match
-function matchCard(card) {
-  const cardsMatched = document.querySelectorAll(".turn");
-  for (let i = 0; i < cardsMatched.length; i++) {
-    cardsMatched[i].classList.remove("turn");
-    cardsMatched[i].classList.add("match");
-  }
-  const totalMatched = document.querySelectorAll(".match").length;
-  if (totalMatched === nCards) {
-    setTimeout(endOfGame, delayInMilliseconds);
-  }
-}
-
-// Variaveis para cartas de cada round
-let firstCardBack = 0;
-let secondCardBack = 0;
-function checkCard(card) {
-  // Considera apenas os clicks de cartas que nao formaram par nem estao viradas
-  if (!card.classList.contains("match") && !card.classList.contains("turn")) {
-    turnCard(card);
-    if (isFirstCard) {
-      firstCardBack = parrotName(card);
-      console.log(`1 - ${firstCardBack}`);
-      isFirstCard = false;
-    } else {
-      secondCardBack = parrotName(card);
-      console.log(`2 - ${secondCardBack}`);
-      if (firstCardBack === secondCardBack) {
-        matchCard(card);
-      } else {
-        setTimeout(unturnCard, delayInMilliseconds, card);
-      }
-      roundsCounter++; //incremeta round
-      isFirstCard = true; //condicao para proxima rodada
-    }
-  }
-}
-
-function endOfGame() {
-  alert(`Fim de Jogo
-  Numero de tentativas: ${roundsCounter}`);
-}
+// *****************************************
+// *****************************************
+// Iniciar o jogo
+// *****************************************
+// *****************************************
+startParrotGame();
